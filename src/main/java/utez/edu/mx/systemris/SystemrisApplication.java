f83cd766386d732f53058236da9b2de3b79c8a49
Torres9429 <20233tn105@utez.edu.mx>
2025-01-08 11:04:32 -0600

    inicio del proyecto

---
+++ src/main/java/utez/edu/mx/systemris/SystemrisApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.systemris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/mx/systemris/SystemrisApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.systemris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
